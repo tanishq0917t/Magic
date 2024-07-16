ec50f5c7fe9749d953fd36e5caf32021867c4e1d
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-07-16 11:20:12 +0530

    Update mlfw_mat_double.h

--- tanishq/include/matrix/mlfw_mat_double.h
+++ tanishq/include/matrix/mlfw_mat_double.h
@@ -5,4 +5,6 @@ typedef struct __mlfw_mat_double mlfw_mat_double;
 mlfw_mat_double * mlfw_mat_double_create_new(dimension_t rows,dimension_t columns);
 void mlfw_mat_double_destroy(mlfw_mat_double *matrix);
 mlfw_mat_double * mlfw_mat_double_from_csv(const char *csv_file_name);
+double mlfw_mat_double_get(mlfw_mat_double *matrix,index_t row,index_t column);
+void mlfw_mat_double_set(mlfw_mat_double *matrix,index_t row,index_t column,double value);
 #endif
