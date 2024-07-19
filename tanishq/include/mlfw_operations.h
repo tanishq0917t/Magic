6b048e75ce81ce489a69a86e84a523776c0d34f4
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-07-19 22:05:49 +0530

    Create mlfw_operations.h

---
+++ tanishq/include/mlfw_operations.h
@@ -0,0 +1,8 @@
+#ifndef __MLFW__OPERATIONS__
+#define __MLFW__OPERATIONS__
+#include<inttypes.h>
+
+#include<mlfw_vector.h>
+#include<mlfw_matrix.h>
+#include<operations/mlfw_operations_double.h>
+#endif
