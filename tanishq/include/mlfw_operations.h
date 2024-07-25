904bd128eb87b0ce58d9f41fcb44a387646cd67c
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-07-25 15:24:24 +0530

    Delete tanishq directory

--- tanishq/include/mlfw_operations.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MLFW__OPERATIONS__
-#define __MLFW__OPERATIONS__
-#include<inttypes.h>
-
-#include<mlfw_vector.h>
-#include<mlfw_matrix.h>
-#include<operations/mlfw_operations_double.h>
-#endif
