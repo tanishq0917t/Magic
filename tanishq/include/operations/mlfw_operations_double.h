0768d38812dddebcb4483ef39ca08311a7213960
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-01 13:02:03 +0530

    Delete tanishq directory

--- tanishq/include/operations/mlfw_operations_double.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MLFW__OPERATIONS__DOUBLE__
-#define __MLFW__OPERATIONS__DOUBLE__
-
-mlfw_column_vector_double * mlfw_multiply_double_row_vector_with_column_vector(mlfw_row_vector_double *left_vector,mlfw_column_vector_double *right_vector);
-mlfw_column_vector_double * mlfw_multiply_double_matrix_with_column_vector(mlfw_mat_double *left_matrix,mlfw_column_vector_double *right_vector);
-mlfw_column_vector_double * mlfw_subtract_double_column_vector(mlfw_column_vector_double *left_vector,mlfw_column_vector_double *right_vector);
-#endif
--- tanishq/o_files/create_lib.sh
+++
@@ -1,8 +0,0 @@
-rm *.o
-rm ../lib/libtanishq.a
-
-gcc -c ../src/matrix/*.c -I ../include -Wall
-gcc -c ../src/vector/*.c -I ../include -Wall
-gcc -c ../src/operations/*.c -I ../include -Wall
-
-ar rcs ../lib/libtanishq.a *.o
