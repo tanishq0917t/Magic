198096b3b82fb439f9d5a5e1cfcb599761cbc684
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-05 19:25:25 +0530

    Create mlfw_mat_string.h

---
+++ tanishq/include/matrix/mlfw_mat_string.h
@@ -0,0 +1,12 @@
+#ifndef __MLFW__MAT__STRING__
+#define __MLFW__MAT__STRING__
+
+struct __mlfw_mat_string;
+typedef struct __mlfw_mat_string mlfw_mat_string;
+mlfw_mat_string * mlfw_mat_string_create_new(dimension_t rows,dimension_t columns);
+void mlfw_mat_string_destroy(mlfw_mat_string *matrix);
+mlfw_mat_string * mlfw_mat_string_from_csv(const char *csv_file_name);
+void mlfw_mat_string_get(mlfw_mat_string *matrix,index_t row,index_t column,char **string);
+void mlfw_mat_string_set(mlfw_mat_string *matrix,index_t row,index_t column,char *string);
+void mlfw_mat_string_get_dimension(mlfw_mat_string *matrix,dimension_t *rows,dimension_t *columns);
+#endif
