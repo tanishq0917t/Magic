2c94e7d9f737f4d3a3eb315a173792ad2073ffc0
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-06 18:08:07 +0530

    Create mlfw_set.h

---
+++ tanishq/include/mlfw_set.h
@@ -0,0 +1,9 @@
+#ifndef __MLFW__SET__
+#define __MLFW__SET__
+#include<inttypes.h>
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+#include <set/mlfw_set_string.h>
+#endif
