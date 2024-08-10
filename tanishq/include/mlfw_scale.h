bbd4caa993486dd7e8d6c549dc143e500d7f47ba
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-10 13:20:09 +0530

    Add files via upload

---
+++ tanishq/include/mlfw_scale.h
@@ -0,0 +1,9 @@
+#ifndef __MLFW__SCALE__
+#define __MLFW__SCALE__
+#include<inttypes.h>
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+#include <scaling/mlfw_scale_double.h>
+#endif
---
+++ tanishq/include/scaling/mlfw_scale_double.h
@@ -0,0 +1,5 @@
+#ifndef __MLFW__SCALE__DOUBLE__
+#define __MLFW__SCALE__DOUBLE__
+#include<mlfw_matrix.h>
+mlfw_mat_double *mlfw_scale_double_min_max(mlfw_mat_double *matrix,index_t start_r,index_t start_c,index_t end_r,index_t end_c);
+#endif
