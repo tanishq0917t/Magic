1b1c5ead19e192465500ac6cf69c9cf284071850
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-10 13:20:39 +0530

    Update create_lib.sh

--- tanishq/o_files/create_lib.sh
+++ tanishq/o_files/create_lib.sh
@@ -6,5 +6,6 @@ gcc -c ../src/vector/*.c -I ../include -Wall
 gcc -c ../src/operations/*.c -I ../include -Wall
 gcc -c ../src/set/*.c -I ../include -Wall
 gcc -c ../src/encoder/*.c -I ../include -Wall
+gcc -c ../src/scaling/*.c -I ../include -Wall
 
 ar rcs ../lib/libtanishq.a *.o
