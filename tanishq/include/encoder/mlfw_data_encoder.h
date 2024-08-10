3f0cc61fa995f522e2f7280ccf2cd3ab74666668
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-10 12:25:18 +0530

    Add files via upload

---
+++ tanishq/include/encoder/mlfw_data_encoder.h
@@ -0,0 +1,4 @@
+#ifndef __MLFW__DATA__ENCODER__
+#define __MLFW__DATA__ENCODER__
+void mlfw_encoder_encode_one_hot(char *source,char *target,int *encode_columns,int size,int header);
+#endif
---
+++ tanishq/include/mlfw_encoder.h
@@ -0,0 +1,10 @@
+#ifndef __MLFW__ENCODE__
+#define __MLFW__ENCODE__
+#include<inttypes.h>
+#ifndef dimension_t
+#define dimension_t uint32_t
+#define index_t uint32_t
+#endif
+
+#include <encoder/mlfw_data_encoder.h>
+#endif
