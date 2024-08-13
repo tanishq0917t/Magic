be6740a9e62ddf298bc45c784125193b024527d5
Tanishq Rawat <67191912+tanishq0917t@users.noreply.github.com>
2024-08-13 15:19:23 +0530

    Update mlfw_vector_double.h

--- tanishq/include/vector/mlfw_vector_double.h
+++ tanishq/include/vector/mlfw_vector_double.h
@@ -15,7 +15,8 @@ mlfw_column_vector_double * mlfw_column_vector_create_new_filled(dimension_t siz
 mlfw_row_vector_double * mlfw_column_vector_double_transpose(mlfw_column_vector_double *vector);
 dimension_t mlfw_column_vector_double_get_size(mlfw_column_vector_double *vector);
 double mlfw_column_vector_double_mean(mlfw_column_vector_double *vector);
-
+void mlfw_column_vector_double_to_csv(mlfw_column_vector_double *vector,char *CSV_FILE_NAME);
+mlfw_column_vector_double * mlfw_column_vector_double_from_csv(char *CSV_FILE_NAME);
 
 //Row vector functions
 mlfw_row_vector_double * mlfw_row_vector_double_create_new(dimension_t size);
@@ -26,5 +27,6 @@ mlfw_row_vector_double * mlfw_row_vector_create_new_filled(dimension_t size,doub
 mlfw_column_vector_double * mlfw_row_vector_double_transpose(mlfw_row_vector_double *vector);
 dimension_t mlfw_row_vector_double_get_size(mlfw_row_vector_double *vector);
 double mlfw_row_vector_double_mean(mlfw_row_vector_double *vector);
-
+void mlfw_row_vector_double_to_csv(mlfw_row_vector_double *vector,char *CSV_FILE_NAME);
+mlfw_row_vector_double * mlfw_row_vector_double_from_csv(char *CSV_FILE_NAME);
 #endif
