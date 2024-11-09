c7fb15da8aad0336f0ec86bad4a19e9da23472be
tanishq0917t <tanishqrawat8@gmail.com>
2024-11-09 13:27:36 +0530

    Refactoring min_max_scaler

--- tools/src/min_max_scaler.c
+++ tools/src/min_max_scaler.c
@@ -1,5 +1,6 @@
 #include<mlfw_scale.h>
 #include<mlfw_matrix.h>
+#include<mlfw_vector.h>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,6 +8,7 @@ int main(int argc,char *argv[])
 {
     dimension_t matrix_rows,matrix_columns;
     mlfw_mat_double *matrix,*scaled_matrix;
+    mlfw_row_vec_string *header;
     char *input_file,*output_file,*min_max_file;
     index_t from_column,to_column;
     if(argc!=6)
@@ -19,16 +21,16 @@ int main(int argc,char *argv[])
     from_column=atoi(argv[3]);
     to_column=atoi(argv[4]);
     min_max_file=argv[5];
-    matrix=mlfw_mat_double_from_csv(input_file);
+    matrix=mlfw_mat_double_from_csv(input_file,NULL,&header);
     if(matrix==NULL) return 0;
     mlfw_mat_double_get_dimension(matrix,&matrix_rows,&matrix_columns);
-    scaled_matrix=mlfw_scale_double_min_max(matrix,0,from_column,matrix_rows-1,to_column,min_max_file);
+    scaled_matrix=mlfw_scale_double_min_max(matrix,0,from_column,matrix_rows-1,to_column,min_max_file,NULL);
     if(scaled_matrix==NULL) 
     {
         mlfw_mat_double_destroy(matrix);
         return 0;
     }
-    mlfw_mat_double_to_csv(scaled_matrix,output_file);
+    mlfw_mat_double_to_csv(scaled_matrix,output_file,header);
     mlfw_mat_double_destroy(matrix);
     mlfw_mat_double_destroy(scaled_matrix);
     return 0;
--- tools/src/min_max_test_scaler.c
+++ tools/src/min_max_test_scaler.c
@@ -1,5 +1,6 @@
 #include<mlfw_scale.h>
 #include<mlfw_matrix.h>
+#include<mlfw_vector.h>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,6 +8,7 @@ int main(int argc,char *argv[])
 {
     dimension_t matrix_rows,matrix_columns;
     mlfw_mat_double *matrix,*scaled_matrix,*min_max_matrix;
+    mlfw_row_vec_string *mat_header,*min_max_header;
     char *input_file,*output_file,*min_max_file;
     index_t from_column,to_column;
     if(argc!=6)
@@ -19,23 +21,23 @@ int main(int argc,char *argv[])
     from_column=atoi(argv[3]);
     to_column=atoi(argv[4]);
     min_max_file=argv[5];
-    matrix=mlfw_mat_double_from_csv(input_file);
+    matrix=mlfw_mat_double_from_csv(input_file,NULL,&mat_header);
     if(matrix==NULL) return 0;
     mlfw_mat_double_get_dimension(matrix,&matrix_rows,&matrix_columns);
-    min_max_matrix=mlfw_mat_double_from_csv(min_max_file);
+    min_max_matrix=mlfw_mat_double_from_csv(min_max_file,NULL,&min_max_header);
     if(min_max_matrix==NULL) 
     {
         mlfw_mat_double_destroy(matrix);
         return 0;
     }
-    scaled_matrix=mlfw_scale_double_min_max_with_given_file(matrix,0,from_column,matrix_rows-1,to_column,min_max_matrix);
+    scaled_matrix=mlfw_scale_double_min_max_with_given_file(matrix,0,from_column,matrix_rows-1,to_column,min_max_matrix,NULL);
     if(scaled_matrix==NULL) 
     {
         mlfw_mat_double_destroy(matrix);
         mlfw_mat_double_destroy(min_max_matrix);
         return 0;
     }
-    mlfw_mat_double_to_csv(scaled_matrix,output_file);
+    mlfw_mat_double_to_csv(scaled_matrix,output_file,mat_header);
 
     mlfw_mat_double_destroy(matrix);
     mlfw_mat_double_destroy(scaled_matrix);
